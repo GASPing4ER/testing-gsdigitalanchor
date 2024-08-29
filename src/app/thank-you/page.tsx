24268b7957603f851298866ea956478ceb7f62cd
GASPing4ER <simecgasper@gmail.com>
2024-08-29 13:07:26 +0200

    testing google ads conversion tracking

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,20 +1,32 @@
 import { cormorant } from "@/src/lib/fonts";
 import Link from "next/link";
+import Script from "next/script";
 
 const ThankYouPage = () => {
   return (
-    <div className="min-h-screen w-full p-24 flex flex-col gap-10 items-center justify-center bg-slate-50 text-slate-900 text-center">
-      <h1 className={`${cormorant.className} text-5xl sm:text-7xl`}>
-        Thank You!
-      </h1>
-      <p>We have received your message and will get back to you shortly.</p>
-      <Link
-        href="/"
-        className="bg-slate-900 text-slate-50 px-6 py-2 rounded-xl"
-      >
-        Return back home
-      </Link>
-    </div>
+    <>
+      <head>
+        <Script id="conversion-tracking" strategy="lazyOnload">
+          {`
+          window.gtag("event", "conversion_event_submit_lead_form", {
+            send_to: "AW-CONVERSION_ID/CONVERSION_LABEL",
+          });
+        `}
+        </Script>
+      </head>
+      <div className="min-h-screen w-full p-24 flex flex-col gap-10 items-center justify-center bg-slate-50 text-slate-900 text-center">
+        <h1 className={`${cormorant.className} text-5xl sm:text-7xl`}>
+          Thank You!
+        </h1>
+        <p>We have received your message and will get back to you shortly.</p>
+        <Link
+          href="/"
+          className="bg-slate-900 text-slate-50 px-6 py-2 rounded-xl"
+        >
+          Return back home
+        </Link>
+      </div>
+    </>
   );
 };
 
