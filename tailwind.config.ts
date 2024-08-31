337942e8d8c871ba893bd936507d262571b049dc
GASPing4ER <simecgasper@gmail.com>
2024-08-31 13:44:11 +0200

    portfolio lautus mobile optimization

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,8 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "hero-pattern": "url('/hero-2.png')",
+        "hero-pattern-mobile": "url('/hero-2.png')",
+        "hero-pattern-desktop": "url('/hero-desktop.png')",
         "pricing-pattern": "url('/background-test.png')",
         "about-pattern": "url('/about.png')",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
