e4c26e561fb145410ffc3e02ca1055b1faa80ba6
GASPing4ER <simecgasper@gmail.com>
2025-01-03 12:08:05 +0100

    pixel connection text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,6 +54,24 @@ export default function RootLayout({
         <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID as string} />
       </head>
       <body className={montserrat.className}>
+        <Script
+          id="facebook-pixel-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window,document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1529155131120875'); 
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
         <Header />
         {children}
         <BeforeFooter />
