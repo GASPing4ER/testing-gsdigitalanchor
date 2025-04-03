e84058c842a62c8046db2394b8f89f77b5267cd6
GASPing4ER <simecgasper@gmail.com>
2025-04-03 12:15:00 +0200

    testing order api

--- src/components/email-template.tsx
+++ src/components/email-template.tsx
@@ -20,22 +20,22 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   <div>
     <h2>You received a new inquiry</h2>
     <p>
-      <strong>Name:</strong> ${name}
+      <strong>Name:</strong> {name}
     </p>
     <p>
-      <strong>Email:</strong> ${email}
+      <strong>Email:</strong> {email}
     </p>
     <p>
-      <strong>Brand:</strong> ${brand}
+      <strong>Brand:</strong> {brand}
     </p>
     <p>
-      <strong>Services:</strong> ${services}
+      <strong>Services:</strong> {services}
     </p>
     <p>
-      <strong>Budget:</strong> ${budget}
+      <strong>Budget:</strong> {budget}
     </p>
     <p>
-      <strong>Message:</strong> ${message}
+      <strong>Message:</strong> {message}
     </p>
   </div>
 );
